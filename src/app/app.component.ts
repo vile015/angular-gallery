167eed1df795548a9e04a8590e423e4a6d280c6a
rhen <renericorielsepulveda1990@gmail.com>
2023-04-09 20:14:14 +0900

    Initialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgVideo from 'lightgallery/plugins/video';
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgShare from 'lightgallery/plugins/share';
+import lgRotate from 'lightgallery/plugins/rotate';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-gallery';
+  title = 'angular-demo';
+  settings = {
+    counter: true,
+    plugins: [lgZoom, lgVideo,lgThumbnail,lgShare,lgRotate]
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+import { NgxGalleryModule } from 'ngx-gallery';
+import { LightgalleryModule } from 'lightgallery/angular';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LightgalleryModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
